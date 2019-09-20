8abc64bbe5bf9f68ca3794da388502f65172aae6
Kanchan Wakchaure <kanchanwakchaure@az1-1026026mbp.3500.dhcp.asu.edu>
2019-09-20 12:22:35 -0700

    Basic UI for sign up

---
+++ frontend/src/components/signup.jsx
@@ -0,0 +1,55 @@
+import React , {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+class Signup extends Component {
+
+
+		render() {
+			return(
+
+            <div>
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="New User"
+           />
+           <TextField
+             hintText="Enter your First Name"
+             floatingLabelText="Firstname"
+             onChange = {(event,newValue) => this.setState({firstname:newValue})}
+             />
+           <br/>
+           <TextField
+             hintText="Enter your Last Name"
+             floatingLabelText="Lastname"
+             onChange = {(event,newValue) => this.setState({lastname:newValue})}
+             />
+           <br/>
+           <TextField
+             hintText="Enter your Username"
+             floatingLabelText="Username"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Sign Up" primary={true} style={style} />
+         </div>
+         </MuiThemeProvider>
+      </div>
+				);
+		}
+}
+
+const style = {
+ margin: 15,
+ };
+
+export default Signup;
