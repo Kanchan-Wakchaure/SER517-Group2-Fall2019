e7a267549ced7e4e1678d7ddc3192e8d80a8e885
Mounika Gadde <VijayaMounika.Gadde@jda.com>
2019-09-20 09:14:41 -0700

    Adding login page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,11 +7,8 @@ import * as serviceWorker from './serviceWorker';
 import "bootstrap/dist/css/bootstrap.css";
 
 import Homepage from './components/homepage';
-
+import login from './components/login'
 
 ReactDOM.render(<Homepage />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<login />, document.getElementById('root'));
 serviceWorker.unregister();
