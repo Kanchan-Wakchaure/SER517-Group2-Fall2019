bdcc1ce89a48b4ea0f66e6815eb9310ea494deb9
raisa-khatun <raisakhatun@gmail.com>
2019-09-19 18:53:30 -0700

    US_10_Task_55 Adding view for creating event list with serializer

--- Travlendar/travlendar_app/views.py
+++ Travlendar/travlendar_app/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
-
+from travlendar_app.models import Event
+from travlendar_app.serializers import EventSerializer
+from rest_framework import generics
 # Create your views here.
-
+'''
 def index(request):
     return HttpResponse("Hello, world. You're at the travlander index.")
+'''
+
+class EventListCreate(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
