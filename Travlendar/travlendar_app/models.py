b0cd10931ae8e181b7578bedf7e4451f26cacaab
raisa-khatun <raisakhatun@gmail.com>
2019-09-19 18:33:06 -0700

    US_10_Task_55 Adding model 'event'

--- Travlendar/travlendar_app/models.py
+++ Travlendar/travlendar_app/models.py
@@ -1,3 +1,7 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=300)
+    time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
