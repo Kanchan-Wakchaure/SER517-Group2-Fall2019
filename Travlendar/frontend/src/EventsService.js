b9038cacbe8811e81caf5c4c5b9a15352000a630
raisa-khatun <raisakhatun@gmail.com>
2019-09-21 00:32:02 -0700

    US_10_Task_55 Adding mapping to 'view' url with frontend for create event

---
+++ Travlendar/frontend/src/EventsService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+const API_URL = 'http://localhost:8000';
+
+export default class EventsService{
+
+    constructor(){}
+
+    createEvent(event){
+        const url = '${API_URL}/api/events/';
+        return axios.post(url,event);
+    }
+
+}
