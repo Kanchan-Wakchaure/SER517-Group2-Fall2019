e8cfc8b0899b9e516602f5ddfa207f1f4183467b
kaustuv deolal <vutsuak96@gmail.com>
2019-09-24 14:12:58 -0700

    initial design of home page

--- frontend/src/components/homepage.jsx
+++ frontend/src/components/homepage.jsx
@@ -1,4 +1,6 @@
 import React , {Component} from 'react';
+import logo from '../log.svg';
+import '../App.css'
 
 class Homepage extends Component {
 
@@ -6,11 +8,19 @@ class Homepage extends Component {
 		render() {
 			return(
 
-				<div>
+				<div className="homepage">
 
-				<h1>Welcome to Travelander</h1>
-				
-				<button> Send </button>
+				<img src={logo} width="115" height="115" />
+				<br/>
+				<h3  style = {{fontFamily : 'PT Serif'}} > TRAVLENDAR </h3>
+				<br/>
+				<br />
+				<button   style={{color: 'white', backgroundColor: '#C53E3E', width : '160px', fontFamily: 'PT Serif'}} > Login </button>
+				<br/>
+				<button style={{color: 'white', backgroundColor: '#C53E3E', width : '160px',  fontFamily: 'PT Serif'}} > Sign Up </button>
+				<br/>
+				<button style={{color: 'white', backgroundColor: '#C53E3E', width : '160px',  fontFamily: 'PT Serif'}} > Social Login </button>
+
 
 				</div>
 
