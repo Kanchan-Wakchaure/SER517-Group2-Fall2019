1350a67b7b538ad761b9533c14bd59fa54d7f085
Kanchan-Wakchaure <you@example.com>
2019-09-24 23:27:30 -0700

    integration of signup page with merge-develop  branch

--- Travlendar/frontend/src/index.js
+++ Travlendar/frontend/src/index.js
@@ -8,11 +8,10 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import Homepage from './components/homepage';
 import CreateEvent from './components/CreateEvent';
+import Signup from './components/signup';
 
-
+ReactDOM.render(<Homepage />, document.getElementById('root'));
 ReactDOM.render(<CreateEvent />, document.getElementById('root'));
+ReactDOM.render(<Signup />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+
