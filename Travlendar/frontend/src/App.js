922483b05690862d141c10768f799567ec2b1f59
Kanchan-Wakchaure <you@example.com>
2019-09-25 00:33:32 -0700

    Integration of signup homepage and meger-develop branch

--- Travlendar/frontend/src/App.js
+++ Travlendar/frontend/src/App.js
@@ -1,43 +1,18 @@
-import React, {Component}  from 'react';
-import { BrowserRouter } from  'react-router-dom'
-import { Route, Link } from  'react-router-dom'
-import CreateEvent from './components/CreateEvent'
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-//import Homepage from './components/homepage';
+import "bootstrap/dist/css/bootstrap.css";
 
-const BaseLayout=()=>(
-	 <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-	  <div  className="content">
+import Homepage from './components/homepage';
 
-        <Route  path="/events/"  component={CreateEvent}  />
-        
-	  </div>
+function App() {
+  return (
+    <div>
+
+    <Homepage />
+
     </div>
-
-)
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <BaseLayout/>
-      </BrowserRouter>
-    );
-  }
+  );
 }
 
 export default App;
--- Travlendar/frontend/src/index.js
+++ Travlendar/frontend/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-//import * as serviceWorker from './serviceWorker';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 import "bootstrap/dist/css/bootstrap.css";
 
 import Homepage from './components/homepage';
-import CreateEvent from './components/CreateEvent';
 import Signup from './components/signup';
+import CreateEvent from './components/CreateEvent';
 
-ReactDOM.render(<Homepage />, document.getElementById('root'));
-ReactDOM.render(<CreateEvent />, document.getElementById('root'));
 ReactDOM.render(<Signup />, document.getElementById('root'));
+ReactDOM.render(<CreateEvent />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
 
 
