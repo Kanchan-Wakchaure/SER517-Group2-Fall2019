cccc267c13a6d7761d6e4541d400e8c3a87b3f80
namrathaov <namrathaov@gmail.com>
2019-10-02 10:49:31 -0700

    [US_62_Task_69] Added additional fields for create event and UI styles

--- Travlendar/frontend/src/components/CreateEvent.js
+++ Travlendar/frontend/src/components/CreateEvent.js
@@ -1,9 +1,9 @@
 //@author Raisa 9-21-19
 import React , {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {AppBar, TextField}  from 'material-ui';
+import {AppBar}  from 'material-ui';
+import {FormGroup, FormLabel, Input, Button, TextField}  from '@material-ui/core';
 import EventsService from '../EventsService';
-import {FormGroup, FormLabel, Input, Button}  from '@material-ui/core';
 
 const eventService=new EventsService();
 
@@ -12,9 +12,11 @@ class CreateEvent extends React.Component {
         super(props);
         this.state = {
             eventDetails: {
-                name: '',
-                location: '',
-                date: ''
+                title: '',
+                date: '',
+                duration: '',
+                source: '',
+                destination: ''
             }
         };
 		this.handleSubmit = this.handleSubmit.bind(this);
@@ -23,12 +25,14 @@ class CreateEvent extends React.Component {
 
 	//https://www.digitalocean.com/community/tutorials/how-to-build-a-modern-web-application-to-manage-customer-information-with-django
 	handleCreate(){
-	//Sending dummy data to server
+
 	eventService.createEvent(
           {
-            "name": this.state.eventDetails.name,
-            "location": this.state.eventDetails.location,
+            "title": this.state.eventDetails.name,
             "date": this.state.eventDetails.date,
+            "duration": this.state.eventDetails.duration,
+            "source": this.state.eventDetails.source,
+            "destination": this.state.eventDetails.destination
         }          
         ).then((result)=>{
           alert("Event created!");
@@ -50,27 +54,37 @@ class CreateEvent extends React.Component {
 
 	render() {
 		return(
-			<div>
+			<div className="createEvent">
                 <MuiThemeProvider>
                     <FormGroup>
-                    <FormLabel htmlFor="name">Event Title</FormLabel> &nbsp;&nbsp;
-                    <Input type="text" name="name" id="name" required
-                        value={this.state.eventDetails.name}
-                        onChange={event => this.handleInputChange(event, 'name')}/>
+                    <TextField name="description" id="description" required
+                        label="Description"
+                        value={this.state.eventDetails.title}
+                        onClick={this.boxClick}
+                        onChange={event => this.handleInputChange(event, 'title')}/>
                    </FormGroup><br/>
                    <FormGroup>
-                   <FormLabel htmlFor="location">Location</FormLabel> &nbsp;&nbsp;
-                   <Input type="text" name="location" id="location" required
-                        value={this.state.eventDetails.location}
-                        onChange = {event => this.handleInputChange(event, 'location')} />
-                   </FormGroup><br/>
-                   <FormGroup>
-                   <FormLabel>Event Date</FormLabel>&nbsp;&nbsp;
-                   <Input type="date" name="date" id="date" required
+                   <TextField type="date" name="date" id="date"
+                        label = "Date" required
                         value={this.state.eventDetails.date}
                         onChange = {event => this.handleInputChange(event, 'date')}/>
                    </FormGroup><br/>
-                   <Button onClick={this.handleSubmit} style={style}> Send </Button>
+                   <FormGroup>
+                   <TextField type="number" name="duration" id="duration" required
+                        label ="Duration (minutes)" value={this.state.eventDetails.duration}
+                        onChange = {event => this.handleInputChange(event, 'duration')}/>
+                   </FormGroup><br/>
+                   <FormGroup>
+                   <TextField type="text" name="source" id="source" required
+                        label = "Source" value={this.state.eventDetails.source}
+                        onChange = {event => this.handleInputChange(event, 'source')} />
+                   </FormGroup><br/>
+                   <FormGroup>
+                   <TextField type="text" name="destination" id="destination" required
+                        label = "Destination" value={this.state.eventDetails.destination}
+                        onChange = {event => this.handleInputChange(event, 'destination')} />
+                   </FormGroup><br/>
+                   <Button onClick={this.handleSubmit} style={style}> Create Event </Button>
                 </MuiThemeProvider>
             </div>
 			);
