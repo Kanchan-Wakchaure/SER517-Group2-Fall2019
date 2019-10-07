ec89abef0774006af72a58d9d26499b0b1fe641b
gaddevmounika <46894014+gaddevmounika@users.noreply.github.com>
2019-10-06 17:22:11 -0700

    Navigation from signup to login and made the UI better

--- Travlendar/frontend/src/components/login.js
+++ Travlendar/frontend/src/components/login.js
@@ -12,111 +12,119 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import './login.css';
+import LoginService from '../Services/LoginService';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+
+const loginService = new LoginService();
+
+class Login extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userDetails: {
+                email: '',
+                password: ''
+            }
+        };
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handlechange = this.handlechange.bind(this);
+	}
+
+    handleRegister(){
+        console.log(this.state.userDetails);
+        loginService.login(
+            {
+                "email": this.state.userDetails.email,
+                "password": this.state.userDetails.password
+            }
+        ).then((result)=>{
+          alert("You have logged in successfully!");
+        }).catch(()=>{
+          alert('There was an error! Please re-check your Details.');
+        });
+
+    }
+
+    handleSubmit(user){
+        user.preventDefault();
+        this.handleRegister();
+    }
+
+
+
+    handlechange(user, inputPropName)
+    {
+        const newState = Object.assign({}, this.state);
+        newState.userDetails[inputPropName] = user.target.value;
+        this.setState(newState);
+    }
+
+    render(){
+
+        return(
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className="paper">
+                    <form className="form" noValidate>
+                        <Grid container spacing={2} style={{backgroundColor: 'white', margin:'-60px'}} >
+                            <Grid item xs={12} align="center">
+                                <Avatar className="avatar">
+                                    <LockOutlinedIcon />
+                                </Avatar>
+                            </Grid>
+                            <Grid item xs={12} align="center">
+                                <Typography component="h1" variant="h5" >
+                                    Login
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField variant="outlined"
+                                           required
+                                           fullWidth
+                                           id="email"
+                                           label="Email Address"
+                                           name="email"
+                                           autoComplete="email"
+                                           value={this.state.userDetails.email}
+                                           onChange = { user => this.handlechange(user, 'email') }/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField variant="outlined"
+                                           required
+                                           fullWidth
+                                           name="password"
+                                           label="Password"
+                                           type="password"
+                                           id="password"
+                                           autoComplete="current-password"
+                                           value={this.state.userDetails.password}
+                                           onChange = { user => this.handlechange(user, 'password') }/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Button type="submit"
+                                        fullWidth
+                                        variant="contained"
+                                        color="primary"
+                                        className="submit"
+                                        onClick={this.handleSubmit}>
+                                    Login
+                                </Button>
+                            </Grid>
+
+                            <Grid item align="center">
+                                <Link href="/signup" variant="body2">
+                                    Not Registered yet? Sign up
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </Container>
+        );
+    }
 }
 
-const useStyles = makeStyles(theme => ({
-  '@global': {
-    body: {
-      backgroundColor: theme.palette.common.white,
-    },
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
-export default function SignIn() {
-  const classes = useStyles();
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+export default Login;
