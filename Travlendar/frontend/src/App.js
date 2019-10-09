07f3a009c2833f8360a32eac581d9b2c7c557857
kaustuv deolal <vutsuak96@gmail.com>
2019-10-09 12:45:58 -0700

    MAP component added

--- Travlendar/frontend/src/App.js
+++ Travlendar/frontend/src/App.js
@@ -11,7 +11,7 @@ import MAP from './components/map';
 import background from './background_image.jpg'
 
 
-/***const style_background = {
+const style_background = {
 
 width:'100%',
 height:'100%',
@@ -19,14 +19,14 @@ position: 'fixed',
 backgroundImage: `url(${background})`,
 backgroundSize: 'cover'  
 };
-**/
+
 
 function App() {
   return (
-    //<div  style={style_background} >
+    <div  style={style_background} >
 
-    <MAP />
-    //</div>
+    <Homepage />
+    </div>
   );
 }
 
