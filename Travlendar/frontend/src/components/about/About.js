9e1287705a8f59042022d4cabd91e8fb19572a66
namrathaov <namrathaov@gmail.com>
2019-11-01 20:44:46 -0700

    [US_108_Task_109] Adding about component folder.

---
+++ Travlendar/frontend/src/components/about/About.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import './About.css';
+/*
+    Author: Namratha Olety Venkatesh
+    Date: 11-01-2019
+    Description: About page for Travlendar
+    References: https://material-ui.com
+*/
+class About extends Component{
+
+    constructor(props) {
+        super(props);
+    }
+    render(){
+            return (
+            <div>
+    <br /><br />
+                <h1>This is about page.</h1>
+            </div>);
+    }
+}
+export default About;
