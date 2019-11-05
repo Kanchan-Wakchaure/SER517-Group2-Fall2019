bfc7c74e062e8082bd31869dc33c27bfb27a104b
namrathaov <namrathaov@gmail.com>
2019-11-04 21:50:11 -0700

    [US_108_Task_109] Minor changes in reformatting.

--- Travlendar/frontend/src/components/about/About.js
+++ Travlendar/frontend/src/components/about/About.js
@@ -4,7 +4,7 @@
     Description: About page for Travlendar
     References: https://material-ui.com/getting-started/templates/pricing/
 */
-import AppBar from '@material-ui/core/AppBar';
+
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -12,8 +12,6 @@ import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import StarIcon from '@material-ui/icons/StarBorder';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
@@ -28,40 +26,11 @@ import mounika from './images/mounika.jpg';
 import kaustuv from './images/kaustuv.jpg';
 import raisa from './images/raisa.jpg';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
 const useStyles = makeStyles(theme => ({
   '@global': {
     body: {
       backgroundColor: theme.palette.common.white,
-    },
-    ul: {
-      margin: 0,
-      padding: 0,
-    },
-    li: {
-      listStyle: 'none',
-    },
-  },
-  appBar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
-  toolbar: {
-    flexWrap: 'wrap',
-  },
-  toolbarTitle: {
-    flexGrow: 1,
+    }
   },
   link: {
     margin: theme.spacing(1, 1.5),
