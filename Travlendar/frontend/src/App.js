ee9c7721815e0c32b1cbc61570d76786852759a3
Kanchan-Wakchaure <you@example.com>
2019-11-16 01:53:09 -0700

    Added marker and pop up on click

--- Travlendar/frontend/src/App.js
+++ Travlendar/frontend/src/App.js
@@ -11,7 +11,8 @@ import ListEvent from './components/eventsList/ListEvent';
 import { connect } from 'react-redux';
 import * as actions from './store/actions/auth';
 import Map from './components/map/MapView';
-import { HOMEPAGE, SIGNUP, LOGIN, CREATE_EVENT, EVENTS_LIST, EVENTS_MAP, EMAIL, TEXT, ABOUT, PREVIEW_ROUTE } from './Routes.js';
+import MapContainer from './components/map/MapContainer';
+import { HOMEPAGE, SIGNUP, LOGIN, MAPCONTAINER, EVENTS_LIST, EVENTS_MAP, EMAIL, TEXT, ABOUT, PREVIEW_ROUTE } from './Routes.js';
 import Email from './components/email/Email';
 import Text from './components/text/Text';
 import About from './components/about/About';
@@ -39,12 +40,12 @@ class App extends Component{
             <BrowserRouter>
                 <div>
                     <Switch>
-                        <Route exact path={HOMEPAGE} component={Homepage} />
-                        <Route path={SIGNUP} component={Signup} />
-                        <Route path={LOGIN} component={Login} />
-                        <Route path={CREATE_EVENT} component={CreateEvent} />
-                        <Route path={EVENTS_LIST} component={ListEvent}/>
-                        <Route path={EVENTS_MAP} component={Map}/>
+                      <Route exact path={HOMEPAGE} component={Homepage} />
+                      <Route path={SIGNUP} component={Signup} />
+                      <Route path={LOGIN} component={Login} />
+                      <Route path={MAPCONTAINER} component={MapContainer} />
+                      <Route path={EVENTS_LIST} component={ListEvent}/>
+                      <Route path={EVENTS_MAP} component={Map}/>
                       <Route path={EMAIL} component={Email}/>
                       <Route path={TEXT} component={Text}/>
                       <Route path={ABOUT} component={About}/>
