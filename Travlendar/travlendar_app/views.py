4084dfc3a3de555c9fdac87c2490c5ea4b10e8a8
Kanchan-Wakchaure <you@example.com>
2019-12-02 00:30:49 -0700

    Cleaned backend code

--- Travlendar/travlendar_app/views.py
+++ Travlendar/travlendar_app/views.py
@@ -27,7 +27,6 @@ def EventList(request):
     if request.method == 'POST':
         modela = apps.get_model('users', 'CustomUser')
         b = modela.objects.get(email=request.user)
-        print("USER:   ",request.user)
 
         if serializer.is_valid():
             serializer.validated_data["creator"] = b
@@ -43,7 +42,6 @@ def EventList(request):
 
             # Finding the nearest previous event and next event from the event that will be created.
             for item in Event.objects.filter(creator_id=getattr(b, 'id')).filter(date=curr_date):
-                print("time: ",item.time,"  date:",item.date)
                 #prev_event=item
                 prev_time = item.time
                 curr_time = serializer.validated_data.get("time")
@@ -52,26 +50,18 @@ def EventList(request):
                 curr_time_delta=datetime.combine(date.min,curr_time ) - datetime.min
                 total_prev = prev_time_delta+prev_duration
                 total_prev_delta=float(total_prev.total_seconds())
-                print("curr time delta: ",curr_time_delta.total_seconds())
-                print("prev time delta: ",prev_time_delta.total_seconds())
-                print("prev duration: ",prev_duration.total_seconds())
-                print("total prev time delta: ",total_prev_delta)
                 diff_delta=abs(float(curr_time_delta.total_seconds())) - abs(total_prev_delta)
-                print("curr-prev", diff_delta)
 
                 if diff_delta>0:
                     if diff_delta< min_diff:
                         min_diff=diff_delta
                         prev_event = item
                         p=1
-                        print("prev event time:",prev_event.time)
-                        print("min_diff:",min_diff)
                 else:
                     if abs(diff_delta)<min_diff2:
                         min_diff2=abs(diff_delta)
                         next_event=item
                         n=1
-                print(" ")
 
             # assiging longitude and latitude to the event that being created.
             f=findLongLat(serializer)
@@ -80,31 +70,23 @@ def EventList(request):
 
             # Checking if there is any conflict while creating new event with next event
             if p==0 and n==1:
-                print("next event: ",next_event.time)
                 next_event_time = datetime.combine(date.min, next_event.time) - datetime.min
-                print("curr_lat: ",serializer.validated_data.get("lat"))
-                print("curr_long: ", serializer.validated_data.get("long"))
                 travel_time= reachable(serializer.validated_data.get("lat"), serializer.validated_data.get("long"),next_event.lat,next_event.long )
                 if travel_time==-1:
                     return Response('API',status=status.HTTP_500_INTERNAL_SERVER_ERROR)
                 if travel_time==-2 or travel_time==-3 or travel_time==-4 :
                     return Response('unreachable',status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-                print("travel_time: ", travel_time)
                 if abs(float((curr_time_delta+curr_duration).total_seconds())) >= abs(float((next_event_time).total_seconds())):
                     return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
                 if abs(float((curr_time_delta + curr_duration).total_seconds())+travel_time) >= abs(float((next_event_time).total_seconds())):
                     return Response('next',status=status.HTTP_412_PRECONDITION_FAILED)
                 #findLongLat(serializer)
                 serializer.save()
-                print("Event created")
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
             # Checking if there is any conflict while creating new event with previous event
             if p==1 and n==0:
-                print("prev event: ", prev_event.time)
                 prev_event_time = datetime.combine(date.min, prev_event.time) - datetime.min
-                print("prev event lat: ", prev_event.lat)
-                print("prev event long: ", prev_event.long)
                 travel_time = reachable(prev_event.lat, prev_event.long,serializer.validated_data.get("lat"), serializer.validated_data.get("long"))
 
                 if travel_time==-1:
@@ -121,13 +103,10 @@ def EventList(request):
 
                 #findLongLat(serializer)
                 serializer.save()
-                print("Event created")
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
             # Checking if there is any conflict while creating new event with both previous event and next event
             if p==1 and n==1:
-                print("prev event: ", prev_event.time)
-                print("next event: ", next_event.time)
                 next=0
                 prev=0
                 prev_event_time = datetime.combine(date.min, prev_event.time) - datetime.min
@@ -170,19 +149,16 @@ def EventList(request):
                     return Response('both', status=status.HTTP_412_PRECONDITION_FAILED)
 
                 serializer.save()
-                print("Event created")
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
             # If there is no events in db on that day
             if p==0 and n==0:
                 #findLongLat(serializer)
                 serializer.save()
-                print("Event created")
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         # when data can not be serialized.
         else:
-            print(serializer.errors)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -191,13 +167,10 @@ def EventList(request):
         modela = apps.get_model('users', 'CustomUser')
         b = modela.objects.get(email=request.user)
         day=request.query_params.get('date')
-        print("Date:", day)
         if not day:
-            print("Inside if")
             day = date.today()
 
         #day = date.today()
-        print("Today:",day)
         event_list = Event.objects.filter(creator_id=getattr(b, 'id')).filter(date=day).order_by('time')
         serializer = EventSerializer(event_list, context={'request': request}, many=True)
         
@@ -212,7 +185,6 @@ def EventList(request):
 def findLongLat(serializer):
 
     event_location = serializer.validated_data.get("destination")
-    print("event location: ",event_location)
     api_response = requests.get(
         'https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(event_location, get_api_key()))
     api_response_dict = api_response.json()
@@ -240,10 +212,7 @@ def reachable(A_lat,A_long,B_lat,B_long):
     api_key = get_api_key()
     url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins='+str(A_lat)+','+str(A_long)+'&destinations='+str(B_lat)+','+str(B_long)+'&key='+api_key
     r = requests.get(url)
-    print("url: ", url)
     x = r.json()
-    print('status: ',x['status'])
-    print("data: ",x)
     if x['status']=='OK':
         if x['rows'][0]['elements'][0]['status']=='ZERO_RESULTS':
             return -2
@@ -252,7 +221,6 @@ def reachable(A_lat,A_long,B_lat,B_long):
         if x['rows'][0]['elements'][0]['status']=='MAX_ROUTE_LENGTH_EXCEEDED':
             return -4
         duration=x['rows'][0]['elements'][0]['duration']['value']
-        print("duration:",duration)
         return duration
     return -1
 
@@ -282,42 +250,26 @@ def Email(request):
     if request.method == 'GET':
 
         today = DATE
-        print("EMAIL")
 
         modela = apps.get_model('users', 'CustomUser')
-
         
         b = modela.objects.get(email=request.user)
-        print(request.user)
 
         event_list = Event.objects.filter(creator_id=getattr(b, 'id')).filter(date=today).order_by('created_at')
         serializer = EventSerializer(event_list, context={'request': request}, many=True)
        
-
         od = serializer.data
-        print(od)
         
         i = od[-1]
-        print(i)
-
             
         if i['date'] == DATE:
-
-            
-
-            print(i['title'])
-            print(i['time'])
-            print(i['destination'])
-            
             subject = i['title']
             content = '<strong> Appointment at %s time : %s </strong>' % (i['destination'], i['time'])
-            #print(str(request.user))
             receiver = str(request.user)
 
             email_list = []
 
             users_dict_raw = i['notifyUsers']
-            print(users_dict_raw)
             if users_dict_raw != []:
 
                 for e in eval(users_dict_raw):
@@ -329,10 +281,7 @@ def Email(request):
             dt_time = DATE + " " + i['time']
         
             send_email(receiver, subject, content , email_list, dt_time)
-            
 
-        #return Response({'data': serializer.data},status=status.HTTP_200_OK)
-        print(request.user)
         return HttpResponse("Got Email Alert Activation")
     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
@@ -343,37 +292,21 @@ def Email(request):
 @api_view(['GET'])
 def Text(request):
     PHN = '+14808592874'
-
-
-    
-
-
     if request.method == 'GET':
 
         today = DATE
-
-        print("TEXT")
         modela = apps.get_model('users', 'CustomUser')
         b = modela.objects.get(email=request.user)
         event_list = Event.objects.filter(creator_id=getattr(b, 'id'))
         phone = getattr(b, 'phone_number')
-        print(phone)
         paginator = Paginator(event_list, 25)
         page = request.GET.get('page')
         
         event_list = Event.objects.filter(creator_id=getattr(b, 'id')).filter(date=today).order_by('created_at')
         serializer = EventSerializer(event_list, context={'request': request}, many=True)
-        
-        #tz = pytz.timezone('US/Arizona')
-        #d = str(datetime.today()).split(" ")[0]
-        
 
         od = serializer.data
-
-        print(od)
         i = od[-1]
-        #for i in od:
-            
         if i['date'] == DATE:
 
             
@@ -413,7 +346,6 @@ def preview_events(request):
             return Response(status=status.HTTP_404_NOT_FOUND)
         output=[]
         data = serializer.data
-        print(data)
         api_response = requests.get(
         'https://maps.googleapis.com/maps/api/geocode/json?latlng={0}&key={1}'.format(data[0]["lat"]+","+data[0]["long"], get_api_key()))
     api_response_dict = api_response.json()
