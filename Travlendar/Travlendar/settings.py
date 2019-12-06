08e7d9d54f750713c3fde638cbbc8b14df20bfac
Vutsuak16 <Vutsuak96@gmail.com>
2019-12-05 20:20:48 -0700

    bug fixes

--- Travlendar/Travlendar/settings.py
+++ Travlendar/Travlendar/settings.py
@@ -6,6 +6,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+#BASE_DIR = map(lambda x : x**2)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print("PATH:",BASE_DIR)
 
